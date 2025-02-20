9334be3086fbb61db41a3c142012d5b38eed8ade
texchip <100250142+texchip@users.noreply.github.com>
2025-02-20 16:00:09 -0500

    Made first graph.

--- pleclerc_lab5.R
+++ pleclerc_lab5.R
@@ -3,16 +3,15 @@ library(tidyverse)
 
 not.allentown <- read_csv("data/essentia.data.csv")
 allentown <- read_csv("data/essentia.data.allentown.csv")
-feature <- allentown$overall_loudness
 
-feature <- "overall_loudness"
+range.check <- function(feature) {
 allen.feature <- allentown[[feature]]
 not.allentown |>
   group_by(artist) |>
-  summarize(min = min(get(feature)),
-            LF = quantile(get(feature),probs = .25)-1.5*IQR(get(feature)),
-            UF = quantile(get(feature),probs = .75)+1.5*IQR(get(feature)),
-            max = max(get(feature))) |>
+  summarize(min = min(get(feature), na.rm=T),
+            LF = quantile(get(feature),probs = .25, na.rm=T)-1.5*IQR(get(feature),na.rm=T),
+            UF = quantile(get(feature),probs = .75, na.rm=T)+1.5*IQR(get(feature),na.rm=T),
+            max = max(get(feature),na.rm=T)) |>
   mutate(out.of.range = allen.feature < min | allen.feature > max,
          unusual = allen.feature < LF | allen.feature > UF,
          description = case_when(
@@ -20,6 +19,36 @@ not.allentown |>
            unusual ~ "Outlying",
            .default = "Within Range")
          )
+}
 
+numeric.cols <- allentown |>
+  select_if(is.numeric) |>
+  names()
 
+descriptors <- tibble(artist = unique(not.allentown$artist))
+  for(i in 1:length(numeric.cols)){
+    desc.name <- numeric.cols[i]
+    output <- range.check(desc.name)
+    descriptors <- descriptors |>
+      left_join(output |> select(artist, description), by = "artist") |>
+      rename(!!desc.name := description)
+  }
 
+summ.table <- descriptors |>
+  pivot_longer(cols = -artist, names_to = "feature", values_to = "description") |>
+  group_by(artist, description) |>
+  summarize(count = n(), .groups = "drop") |>
+  pivot_wider(names_from = description, values_from = count, values_fill = 0)
+print(summ.table)
+
+summ.table.long <- summ.table |>
+  pivot_longer(cols = -artist, names_to = "range_status", values_to = "count")
+
+ggplot(summ.table.long, aes(x = artist, y = count, fill = range_status)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  geom_hline(yintercept=0) +
+  labs(title = "Count of Descriptor Status for \"Allentown\" between Artists",
+       x = "Artist",
+       y = "Count",
+       fill = "Descriptor Status") +
+  theme_bw()
